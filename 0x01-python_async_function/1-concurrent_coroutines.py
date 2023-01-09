0d709cd490df0d2c30691d33cbc6c43612c2e5a6
sadebare <adebaresulaimon04@gmail.com>
2023-01-09 14:52:08 +0100

    python async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns wait_random n times
